143db76ecabe01408d9d97f65e25f6f6b57b29d7
Dav-Cast <dcastillochamorro@gmail.com>
2022-11-16 12:31:27 -0600

    added modification to the schema for the title table and added the queiries used to gather the retiring information needed.

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -50,9 +50,22 @@ CREATE TABLE titles (
 	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+--	PRIMARY KEY (emp_no)
 );
 
+-- FIX titles TABLE 
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+
+SELECT * FROM titles;
 SELECT * FROM departments;
 SELECT * FROM dept_emp;
